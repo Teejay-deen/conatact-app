4ebd6c7a9f9904ed9e90b4d310a1ae7264da8db6
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-11 11:43:43 +0100

    fixed some bugs

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 const AddContactList = () => {
+  const [state, setState] = useState({});
+  
+  const _state = {
+    name: "",
+    email: "",
+  };
+
   return (
     <div className="ui main">
       <h2>Add Contact</h2>
       <form className="ui form" action="">
-      <div className="field">
-        <label htmlFor="">Name</label>
-        <input name="name" placeholder="Name" type="text" />
-      </div>
+        <div className="field">
+          <label htmlFor="">Name</label>
+          <input
+            name="name"
+            placeholder="Name"
+            type="text"
+            onChange={(e) => {
+              _state.name = e.target.value;
 
-      <div className="field">
-        <label htmlFor="">Email</label>
-        <input name="name" placeholder="Email" type="text" />
-      </div>
+              setState(_state);
+            }}
+          />
+        </div>
 
-      <button className="ui button blue">Add</button>
+        <div className="field">
+          <label htmlFor="">Email</label>
+          <input name="name" placeholder="Email" type="text" />
+        </div>
+
+        <h2>State: {state.name}</h2>
+        <button className="ui button blue">Add</button>
       </form>
     </div>
   );
