a1862cd6ea44495e66b72c8035bf44a22f172a95
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-11 10:03:01 +0100

    using class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import "./App.css";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Tajudeen",
-      email: "Jimoh.tajudeen1017@gmail.com",
-    },
 
-    {
-      id: "2",
-      name: "Jimoh",
-      email: "Jimoh@gmail.com",
-    }
-  ];
+  const [contacts, setContact] =useState([])
+  // const contacts = [
+  //   {
+  //     id: "1",
+  //     name: "Tajudeen",
+  //     email: "Jimoh.tajudeen1017@gmail.com",
+  //   },
+
+  //   {
+  //     id: "2",
+  //     name: "Jimoh",
+  //     email: "Jimoh@gmail.com",
+  //   }
+  // ];
   return (
     <div className="ui container ">
       <Header />
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import user from "../images/Teejay.jpg"
 
 const ContactCard = (props) => {
   const { id, name, email } = props.contact;
   return (
     <div className="item">
+      <img className="ui avatar image" src={user} alt="user" />
       <div className="content">
         <div className="header">{name}</div>
         <div className="header">{email}</div>
       </div>
       <i
-        style={{ color: "red", marginTop: "7px" }}
+        style={{ color: "red", marginTop: "px" }}
         className="trash alternate outline icon "
       ></i>
     </div>
